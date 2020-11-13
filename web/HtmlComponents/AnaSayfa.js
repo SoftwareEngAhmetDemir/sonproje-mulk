92dfaba45788b96e51c2cc5d2e2b3a029f225156
SoftwareEngAhmetDemir <deepdamer@gmail.com>
2020-11-13 15:07:50 +0300

    add

--- web/HtmlComponents/AnaSayfa.js
+++ web/HtmlComponents/AnaSayfa.js
@@ -1,20 +1,30 @@
 import { Tooltip } from "@material-ui/core";
 import Axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
-import { BrowserRouter, Redirect, useHistory } from "react-router-dom";
+import React, { Fragment, useContext, useEffect, useRef, useState } from "react";
+import { BrowserRouter, Link, Redirect, useHistory } from "react-router-dom";
 import TurkeyMap from "turkey-map-react";
 import axios from "axios";
 import "../../web/style.css";
 import Select from "react-select";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/swiper.scss";
-// import "swiper/components/pagination/pagination.scss";
-// import SwiperCore, { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+import "swiper/components/pagination/pagination.scss";
+import SwiperCore, { Pagination } from "swiper";
 
 import { PieChart } from "react-minimal-pie-chart";
-// SwiperCore.use([Pagination]);
+import { useTranslation } from "react-i18next";
+import { useSnackbar } from "notistack";
+import { AuthContext } from "../../panel/Context/AuthContext";
+
+SwiperCore.use([Pagination]);
 
 function AnaSayfa() {
+
+  const [t] = useTranslation();
+  const { enqueueSnackbar } = useSnackbar();
+  const { isAuthenticated, user, setIsAuthenticated, setUser } = useContext(
+    AuthContext
+  );
   var [show_cities, show_cities_f] = useState([]);
   var [chart_show, char_show_f] = useState([]);
   var son_mat = [];
@@ -199,7 +209,7 @@ function AnaSayfa() {
       "/filter?city=" +
         show_cities[index] +
         "&&turu=" +
-        chart_show[index][segmentIndex].title
+        chart_show[index][segmentIndex].turu
     );
   };
 
@@ -210,7 +220,43 @@ function AnaSayfa() {
     selectedCategoryItemValue: "",
     selectedCategoryItemLabel: "",
   });
+  const { gState, seTgState } = useContext(AuthContext);
+  const [userState, setUserState] = useState({
+    username: '',
+    name: '',
+    surname: '',
+    favorites: [],
+    id: '',
+  });
 
+  function getUserData() {
+    if (user && user.id !== '') {
+      if (user.role[0].customers === true) {
+        axios.get(`/staff/customer/${user.id}`).then(async (response) => {
+          await setUserState({
+            ...userState,
+            id: response.data._id,
+            username: response.data.username,
+            name: response.data.name,
+            surname: response.data.surname,
+            favorites: response.data.favorites,
+          });
+        });
+      } else {
+        axios.get(`/staff/${user.id}`).then(async (response) => {
+          await setUserState({
+            ...userState,
+            id: response.data._id,
+            username: response.data.username,
+            name: response.data.name,
+            surname: response.data.surname,
+            favorites: response.data.favorites,
+          });
+        });
+      }
+    }
+  }
+  
   const getEstateData = () => {
     axios.get("/estate/public").then((response) => {
       if (response.data.length > 0) {
@@ -272,13 +318,403 @@ function AnaSayfa() {
   }
   // console.log(chanceCat);
   useEffect(() => {
+    getUserData();
     getEstateCategories();
     getEstateData();
   }, []);
 
   return (
     <div>
-      <button onClick={() => get_isin_Tipleri()}>+++</button>
+{/* ege */}
+
+<section id="mainSlider">
+        <div className="searchBar row container-md px-0">
+          <form>
+            <div className="col-sm-3 col-4  px-0  float-left">
+              <div className="form-control category  " id="select1">
+                <Select
+                  value={category.selectedCategoryItems}
+                  options={category.findCategory}
+                  onChange={handleSelectCategory}
+                />
+              </div>
+            </div>
+            <div className="col-sm-3 col-4  px-0  float-left">
+              <div className="form-control location" id="select2">
+                <Select
+                  value={category.selectedCategoryItems}
+                  options={category.findCategory}
+                  onChange={handleSelectCategory}
+                />
+              </div>
+            </div>
+            <div className="col-sm-3 col-4  px-0  float-left">
+              <div className="form-control room" id="select3">
+                <Select
+                  value={category.selectedCategoryItems}
+                  options={category.findCategory}
+                  onChange={handleSelectCategory}
+                />
+              </div>
+            </div>
+            <div className="col-sm-3 col-12  px-0 float-left">
+              <div className="searchArea">
+                <input
+                  type="search"
+                  id="gsearch"
+                  name="gsearch"
+                  placeholder="Aramak istediğiniz kelimeyi giriniz..."
+                />
+                <button type="submit">
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    className="bi bi-search"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="container justify-content-center">
+          <div className="features owerflow-1200">
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/servicePrice.png" alt="" />
+                <span>Hizmet Bedeli Avantajı</span>
+              </div>
+            </div>
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/kdv.png" alt="" />
+                <span>KDV Avantajı</span>
+              </div>
+            </div>
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/authorMark.png" alt="" />
+                <span>Kurumsal Satıcı</span>
+              </div>
+            </div>
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/tapu.png" alt="" />
+                <span>Temiz Tapu</span>
+              </div>
+            </div>
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/seffaf.png" alt="" />
+                <span>Şeffaf Satış</span>
+              </div>
+            </div>
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/experience.png" alt="" />
+                <span>Mesleki Tecrübe</span>
+              </div>
+            </div>
+            <div className="li">
+              <div className="ov2">
+                <img src="img/icons/profVision.png" alt="" />
+                <span>Profesyonel Görüş</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {!open && (
+        <section id="first-slider">
+          {category.findCategory.map((data) => (
+            <Fragment key={data.value}>
+              <div className="container">
+                <h5 className="sectionTitle">
+                  {data.label}
+                  <span>{data.label}</span>
+                </h5>
+              </div>
+              <div id="sliderFirst" className="slider-wrapper">
+                <div className="container-fluid">
+                  <h1>Lorem ipsum dolor sit amet consectetur</h1>
+                  <span className="text">
+                    Sit amet consectetur masa sed arcu
+                  </span>
+                  <Link to="">Tümüne Göz At</Link>
+                </div>
+                <br />
+                <Swiper spaceBetween={50} slidesPerView={3}>
+                  {chanceCat.map((cData) => {
+                    if (cData === data.value) {
+                      {
+                        return state.map((estate) => (
+                          <SwiperSlide>
+                            <div className="swiper-wrapper">
+                              <div className="swiper-slide" key={estate._id}>
+                                <Link to="">
+                                  <img src="img/product/product.jpg" alt="" />
+                                  <div className="product-summary">
+                                    <div className="summary-top">
+                                      <span className="badge offer left-top">
+                                        Teklif Bekleniyor
+                                      </span>
+                                      <span className="badge oppor right-top">
+                                        Fırsat
+                                      </span>
+                                    </div>
+                                    <div className="summary-bottom">
+                                      <ul>
+                                        <li>{estate.props.Fiyat} TL</li>
+                                        <li>
+                                          <button>
+                                            <i className="fas fa-heart"></i>
+                                          </button>
+                                        </li>
+                                        <li>
+                                          Şekerbank'tan Kocaeli Başiskele'de
+                                          2.400m2 arsa
+                                        </li>
+                                        <li>
+                                          <i className="fas fa-map-marker-alt"></i>
+                                          Kocaeli-Başiskele-Körfez-Arsa-Ticari
+                                          İmarlı
+                                        </li>
+                                        <li>BRG</li>
+                                      </ul>
+                                    </div>
+                                  </div>
+                                </Link>
+                              </div>
+                              <div className="swiper-pagination"></div>
+                            </div>
+                          </SwiperSlide>
+                        ));
+                      }
+                    }
+                  })}
+                </Swiper>
+              </div>
+              <br />
+            </Fragment>
+          ))}
+        </section>
+      )}
+      {open && (
+        <section id="first-slider">
+          {category.selectedCategoryItemLabel && (
+            <div className="container">
+              <h5 className="sectionTitle">
+                {category.selectedCategoryItemLabel}
+                <span>{category.selectedCategoryItemLabel}</span>
+              </h5>
+            </div>
+          )}
+
+          <div id="sliderFirst" className="slider-wrapper">
+            <div className="container-fluid">
+              {category.selectedCategoryItemLabel && (
+                <Fragment>
+                  <br />
+                  <h1>Lorem ipsum dolor sit amet consectetur</h1>
+                  <span className="text">
+                    Sit amet consectetur masa sed arcu
+                  </span>
+                  <Link to="#">Tümüne Göz At</Link>
+                  <br />
+                </Fragment>
+              )}
+
+              <Swiper spaceBetween={50} slidesPerView={4}>
+                {state.map((estate) => {
+                  if (estate.chance_category) {
+                    const categoryArray = estate.chance_category;
+                    return categoryArray.map((item) => {
+                      if (item.value === category.selectedCategoryItemValue) {
+                        return (
+                          <SwiperSlide>
+                            <Link to="#">
+                              <img src="img/product/product.jpg" alt="" />
+                              <div className="product-summary">
+                                <div className="summary-top">
+                                  <span className="badge offer left-top">
+                                    Teklif Bekleniyor
+                                  </span>
+                                  <span className="badge oppor right-top">
+                                    {" "}
+                                    {estate.chance_category
+                                      ? estate.chance_category.map((label) => (
+                                          <Fragment key={label.value}>
+                                            <p>{label.label}</p>
+                                            <hr
+                                              style={{
+                                                marginTop: "0",
+                                                marginBottom: 0,
+                                              }}
+                                            />
+                                          </Fragment>
+                                        ))
+                                      : ""}
+                                  </span>
+                                </div>
+                                <div className="summary-bottom">
+                                  <ul>
+                                    <li>{estate.props.Fiyat}</li>{" "}
+                                    <li>
+                                      <button>
+                                        <i className="fas fa-heart"></i>
+                                      </button>
+                                    </li>
+                                    <li>
+                                      Şekerbank'tan Kocaeli Başiskele'de 2.400m2
+                                      arsa
+                                    </li>
+                                    <li>
+                                      <i className="fas fa-map-marker-alt"></i>
+                                      Kocaeli-Başiskele-Körfez-Arsa-Ticari
+                                      İmarlı
+                                    </li>
+                                    <li>BRG</li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </Link>
+                          </SwiperSlide>
+                        );
+                      }
+                    });
+                  }
+                })}
+              </Swiper>
+            </div>
+          </div>
+        </section>
+      )}
+      <section id="secondSlider">
+        <div className="container-wide">
+          <div id="sliderSecond" className="swiper-container">
+            <div className="swiper-wrapper">
+              <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                pagination={{ clickable: true }}
+              >
+                <SwiperSlide>
+                  <Link className="tarla" to="/filter">
+                    <img
+                      className="first-img"
+                      src="img/icons/Yatitimlik_Arsalar.png"
+                      alt=""
+                    />
+                    <img
+                      className="second-img d-none"
+                      src="img/icons/Yatitimlik_Arsalar1.png"
+                      alt=""
+                    />
+                    <p className="option-title">Yatırımlık Arsalar</p>
+                    <p className="option-desc">
+                      İlgilendiğiniz gayrimenkul ve satış süreçleri hakkında
+                      uzmanlarımız ile konuşun ve bilgi alın.
+                    </p>
+                  </Link>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Link className="konut" to="/filter">
+                    <img
+                      className="first-img"
+                      src="img/icons/satilik-konutlar.png"
+                      alt=""
+                    />
+                    <img
+                      className="second-img d-none"
+                      src="img/icons/satilik-konutlar1.png"
+                      alt=""
+                    />
+                    <p className="option-title">Satılık Konutlar</p>
+                    <p className="option-desc">
+                      İlgilendiğiniz gayrimenkul ve satış süreçleri hakkında
+                      uzmanlarımız ile konuşun ve bilgi alın.
+                    </p>
+                  </Link>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Link className="mustakil" to="/filter">
+                    <img
+                      className="first-img"
+                      src="img/icons/Mustakil-Evler.png"
+                      alt=""
+                    />
+                    <img
+                      className="second-img d-none"
+                      src="img/icons/Mustakil-Evler1.png"
+                      alt=""
+                    />
+                    <p className="option-title">Müstakil Evler</p>
+                    <p className="option-desc">
+                      İlgilendiğiniz gayrimenkul ve satış süreçleri hakkında
+                      uzmanlarımız ile konuşun ve bilgi alın.
+                    </p>
+                  </Link>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Link className="villa" to="/filter">
+                    <img
+                      className="first-img"
+                      src="img/icons/Villalar.png"
+                      alt=""
+                    />
+                    <img
+                      className="second-img d-none"
+                      src="img/icons/Villalar12.png"
+                      alt=""
+                    />
+                    <p className="option-title">Villalar</p>
+                    <p className="option-desc">
+                      İlgilendiğiniz gayrimenkul ve satış süreçleri hakkında
+                      uzmanlarımız ile konuşun ve bilgi alın.
+                    </p>
+                  </Link>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Link className="daire" to="/filter">
+                    <img
+                      className="first-img"
+                      src="img/icons/Daireler.png"
+                      alt=""
+                    />
+                    <img
+                      className="second-img d-none"
+                      src="img/icons/Daireler2.png"
+                      alt=""
+                    />
+                    <p className="option-title">3+1 Daireler</p>
+                    <p className="option-desc">
+                      İlgilendiğiniz gayrimenkul ve satış süreçleri hakkında
+                      uzmanlarımız ile konuşun ve bilgi alın.
+                    </p>
+                  </Link>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+
+
+
+      {/*ege  */}
+      {/* <button onClick={() => get_isin_Tipleri()}>+++</button> */}
       <TurkeyMap
         onClick={(Details) => Go_To_Filter(Details)}
         onHover={(Details) => Ilan_Sayisi_Harita_Uzerinde(Details)}
